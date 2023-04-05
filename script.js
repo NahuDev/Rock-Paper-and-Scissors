c669b72fac66b49030cd63c25dba7f5bf1c6ddcb
Nahu <nahu20dev@gmail.com>
2023-04-05 16:43:24 -0300

    create playRound function

--- script.js
+++ script.js
@@ -3,3 +3,32 @@
 function getComputerChoice(){
     return ["rock","paper","scissors"][Math.floor(3*Math.random())]
 }
+
+function playRound( playerSelection , computerSelection ){
+    if (playerSelection === computerSelection){
+        return "draw";
+    }
+    switch(playerSelection){
+        case "rock":
+            if(computerSelection =="paper"){
+                return "Computer";
+            }else{
+                return "Player";
+            }
+            break;
+        case "paper":
+            if(computerSelection =="scissors"){
+                return "Computer";
+            }else{
+                return "Player";
+            }
+            break;
+        case "scissors":
+            if(computerSelection =="rock"){
+                return "Computer";
+            }else{
+                return "Player";
+            }
+            break;
+    }
+}
